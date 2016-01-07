c0da8e5916b35fb9dbf80e516c4b1bd72d02852f
And Nothing Else <and.nothing.else@ya.ru>
2016-01-07 20:12:21 +0300

    магазины в списке только из выбранного региона.

--- shops/views.py
+++ shops/views.py
@@ -7,6 +7,9 @@ class ShopListView(ListView):
     template_name = 'shops/shop_list.html'
     context_object_name = 'shops'
 
+    def get_queryset(self):
+        return self.request.location.shop_set.all()
+
 
 class ShopDetailView(DetailView):
     model = Shop
