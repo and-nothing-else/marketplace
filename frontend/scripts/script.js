7671f2035bd01ce906baf863dbf8dc0680380008
And Nothing Else <and.nothing.else@ya.ru>
2016-01-19 16:55:22 +0300

    выбор раздела в два шага

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -11,10 +11,24 @@ $("#region_switcher").dropdown({
 
 $("#user_select_item_category").dropdown({
     onChange: function(value, text, $selectedItem) {
+        $("#user_select_item_subcategory")
+            .dropdown('restore default text')
+            .removeClass("disabled")
+            .find(".item")
+            .addClass("filtered")
+            .filter("[data-parent="+value+"]")
+            .removeClass("filtered");
+        $('#button_next')
+            .removeAttr("href")
+            .addClass('disabled');
+}});
+$("#user_select_item_subcategory").dropdown({
+    onChange: function(value, text, $selectedItem) {
         $('#button_next')
             .attr("href", $selectedItem.data("next-url"))
             .removeClass('disabled');
-}});
+    }
+});
 
 
 $(".item_size").popup();
