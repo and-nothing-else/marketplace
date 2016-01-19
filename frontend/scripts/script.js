3faba645217380bd5e8dfbdd5b4311e25961b363
And Nothing Else <and.nothing.else@ya.ru>
2016-01-19 16:55:22 +0300

    выбор раздела в два шага

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -11,10 +11,20 @@ $("#region_switcher").dropdown({
 
 $("#user_select_item_category").dropdown({
     onChange: function(value, text, $selectedItem) {
+        $("#user_select_item_subcategory")
+            .removeClass("disabled")
+            .find(".item")
+            .addClass("filtered")
+            .filter("[data-parent="+value+"]")
+            .removeClass("filtered");
+}});
+$("#user_select_item_subcategory").dropdown({
+    onChange: function(value, text, $selectedItem) {
         $('#button_next')
             .attr("href", $selectedItem.data("next-url"))
             .removeClass('disabled');
-}});
+    }
+});
 
 
 $(".item_size").popup();
