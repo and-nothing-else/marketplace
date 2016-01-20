8654f1f3ae0c8d6d6e54fc69583d92d42d43e4ac
And Nothing Else <and.nothing.else@ya.ru>
2016-01-20 18:59:43 +0300

    фильтр по цвету

--- catalog/filters.py
+++ catalog/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from django.utils.translation import ugettext_lazy as _
 from django.forms.widgets import CheckboxSelectMultiple
-from .models import Item
+from .models import Item, ItemSKU
 
 
 class ItemFilter(django_filters.FilterSet):
@@ -16,7 +16,7 @@ class ItemFilter(django_filters.FilterSet):
 
         def filter(self, qs, value):
             if value:
-                sku_allowed = True
+                sku_allowed = True  # TODO: ну понятно в общем
                 if sku_allowed:
                     return qs.filter(itemsku__itemskusize__standard_size__value__in=value).distinct()
                 else:
@@ -38,9 +38,28 @@ class ItemFilter(django_filters.FilterSet):
             else:
                 return qs
 
+    class ColorFilter(django_filters.Filter):
+        def __init__(self, *args, **kwargs):
+            kwargs.update({
+                'label': _('color'),
+                'widget': CheckboxSelectMultiple,
+            })
+            super().__init__(*args, **kwargs)
+
+        def filter(self, qs, value):
+            if value:
+                sku_allowed = True
+                if sku_allowed:
+                    return qs.filter(itemsku__color__name__in=value).distinct()
+                else:
+                    return qs.filter(color__name__in=value)
+            else:
+                return qs
+
     price = django_filters.RangeFilter()
     size = SizeFilter()
     fabric = FabricFilter()
+    color = ColorFilter()
 
     def __init__(self, data=None, queryset=None, prefix=None, strict=None, category=None):
         super().__init__(data, queryset, prefix, strict)
@@ -53,7 +72,12 @@ class ItemFilter(django_filters.FilterSet):
                 (item['fabric'], item['fabric'])
                 for item in category.item_set.active().values('fabric').order_by('fabric').distinct()
                 ]
-            pass
+            self.filters['color'].field.choices = [
+                (item['color__name'], item['color__name'])
+                for item in ItemSKU.objects.filter(
+                        item__category=category, item__active=True
+                ).values('color__name').order_by('color__name').distinct()
+                ]
 
     class Meta:
         model = Item
